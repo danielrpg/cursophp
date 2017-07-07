3f778c6acbd46ea8b59182f903b54403f0d747da
danielrpg <daniel.fernandezgp@gmail.com>
2017-07-06 22:31:49 -0400

    Primera Clase

---
+++ clase1/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: dany
+ * Date: 7/6/2017
+ * Time: 9:51 PM
+ */
+class Usuario {
+
+    // Propiedades
+    public $nombre;
+
+    // Métodos
+    public function set_usuario() {
+        $this->nombre = 'Juan';
+    }
+}
